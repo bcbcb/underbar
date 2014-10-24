6240751a6a7369e9c5a1db34447ccb05934a55f8
bcbcb <bcbcb@users.noreply.github.com>
2014-10-24 13:28:03 -0700

    _.map done

--- src/underbar.js
+++ src/underbar.js
@@ -117,12 +117,18 @@ var _ = {};
     return array;
   };
 
-
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    
+    _.each(collection, function(item, index) {
+      results.push(iterator(item, index));
+    });
+
+    return results;
   };
 
   /*
